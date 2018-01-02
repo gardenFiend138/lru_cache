f60cd19b997731a727bbe0a0ef5588b31894f63b
Guy Wassather <ggwass@gmail.com>
2018-01-02 12:50:29 -0800

    Finish integer set

--- lib/p01_int_set.rb
+++ lib/p01_int_set.rb
@@ -5,47 +5,60 @@ class MaxIntSet
   end
 
   def insert(num)
+    validate!(num)
+    store[num] = true
   end
 
   def remove(num)
+    validate!(num)
+    store[num] = false
   end
 
   def include?(num)
+    store[num] == true
   end
 
   private
   attr_accessor :store, :size
 
   def is_valid?(num)
+    num <= size && num >= 0
   end
 
   def validate!(num)
+    raise 'Out of bounds' if !is_valid?(num)
   end
 end
 
 
 class IntSet
+  attr_accessor :store 
+  
   def initialize(num_buckets = 20)
-    @store = Array.new(num_buckets) { Array.new }
+    self.store = Array.new(num_buckets) { Array.new }
   end
 
   def insert(num)
+    return false if include?(num)
+    self[num] << num 
   end
 
   def remove(num)
+    self[num].delete(num)
   end
 
   def include?(num)
+    self[num].include?(num)
   end
 
   private
 
   def [](num)
-    # optional but useful; return the bucket corresponding to `num`
+    self.store[num % num_buckets]
   end
 
   def num_buckets
-    @store.length
+    self.store.length
   end
 end
 
